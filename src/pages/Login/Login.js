51baf51fc564221a24c272526c7c396b86703853
an-nguyen-jh <an.nguyen@journeyh.io>
2021-11-16 16:27:56 +0700

    [Style] Add global style, font-family, and frame for login page
    Add global style: font-family, css variable,... in globalStyle.css
    
    Add css style for pages
    
    Create frame for login page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
-
+import "../styles/styles.css";
 class Login extends Component {
   render() {
-    return <h1>Login</h1>;
+    return (
+      <div className="container login-bg-color">
+        <div className="login">
+          <h1 className="login__title">Login</h1>
+          <form className="login__form">
+            <label className="login__form__label" htmlFor="">
+              Username
+            </label>
+            <input type="text" name="username" />
+            <label className="login__form__label" htmlFor="">
+              Password
+            </label>
+            <input type="text" name="password" />
+            <input type="submit" value="Login" />
+          </form>
+        </div>
+      </div>
+    );
   }
 }
 

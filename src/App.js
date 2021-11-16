f083729922fce8a9c69f1fa16222aae913ef4a55
an-nguyen-jh <an.nguyen@journeyh.io>
2021-11-16 12:01:16 +0700

    [Feat] Handle navigate with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Admin from "./pages/Admin/Admin";
+import Employee from "./pages/Employee/Employee";
+import Signin from "./pages/Signin/Signin";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/employee" element={<Employee></Employee>}></Route>
+        <Route exact path="/admin" element={<Admin></Admin>}></Route>
+        <Route exact path="/" element={<Signin></Signin>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Employee/Employee.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Employee extends Component {
+  render() {
+    return <h1>Employee</h1>;
+  }
+}
+
+export default Employee;

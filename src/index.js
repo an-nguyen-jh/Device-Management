433e1b8464eac1b164dfd4ae5850f0ca80c8a288
An Nguyen <91445818+an-nguyen-jh@users.noreply.github.com>
2021-11-16 16:52:06 +0700

    [Fix] Change index.css to globalStyle.css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "./nomalize.css";
-import "./index.css";
+import "./globalStyle.css";
 
 ReactDOM.render(
   <React.StrictMode>

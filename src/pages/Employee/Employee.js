1f204e237f4186894f9e3b7dfcae42235b7a579c
an-nguyen-jh <an.nguyen@journeyh.io>
2021-11-24 09:39:03 +0700

    [Fix] Remove unneccessary Suspense

--- src/pages/Employee/Employee.js
+++ src/pages/Employee/Employee.js
@@ -9,29 +9,21 @@ class Employee extends Component {
       <div className="container page-wrapper">
         <Appbar></Appbar>
         <div className="content">
-          <Suspense
-            fallback={
-              <div className="container">
-                <h1>Loading...</h1>
-              </div>
-            }
-          >
-            <Switch>
-              {employeeSubRouters.map((router) => (
-                <Route
-                  exact
-                  key={router.pathname}
-                  path={`${this.props.match.path}${router.pathname}`}
-                  component={router.component}
-                ></Route>
-              ))}
-              <Route exact path={`${this.props.match.path}`}>
-                <Redirect
-                  to={`${this.props.match.path}${employeeSubRouters[0].pathname}`}
-                />
-              </Route>
-            </Switch>
-          </Suspense>
+          <Switch>
+            {employeeSubRouters.map((router) => (
+              <Route
+                exact
+                key={router.pathname}
+                path={`${this.props.match.path}${router.pathname}`}
+                component={router.component}
+              ></Route>
+            ))}
+            <Route exact path={`${this.props.match.path}`}>
+              <Redirect
+                to={`${this.props.match.path}${employeeSubRouters[0].pathname}`}
+              />
+            </Route>
+          </Switch>
         </div>
       </div>
     );

108cf45f5f4dbddad3806580e420ad901c2fec24
an-nguyen-jh <an.nguyen@journeyh.io>
2021-11-29 18:31:54 +0700

    [Style] Show info in confirm delete dialog

--- src/components/Dialog/ComFirmDeleteDialog.js
+++ src/components/Dialog/ComFirmDeleteDialog.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { Button } from "..";
+import { Alert, Button } from "..";
 import { confirmDialogAction } from "../../store/actions";
 import "../styles/dialog.css";
+import { IoWarningOutline } from "react-icons/io5";
 
 function ConfirmDeleteDialog() {
   const { shouldDisplay, name, email } = useSelector((state) => ({
@@ -18,6 +19,24 @@ function ConfirmDeleteDialog() {
   return shouldDisplay ? (
     <div className="dialog-wrapper">
       <div className="dialog">
+        <div className="dialog__content">
+          <h2 className="grid-span-2"> Confirm delete</h2>
+          <p className="grid-span-2">
+            Are you sure you want to delete this device info ?
+          </p>
+          <p className="dialog__content__label">Employee: </p>
+          <p>{name}</p>
+          <p className="dialog__content__label">Email: </p>
+          <p>{email}</p>
+          <Alert
+            className="grid-span-2"
+            type="error"
+            icon={<IoWarningOutline />}
+          >
+            Deleting this employee's device information will also delete all
+            pending employee's device requests
+          </Alert>
+        </div>
         <div className="dialog__btn-group">
           <Button variant="text" onClick={handleClose}>
             CANCEL
